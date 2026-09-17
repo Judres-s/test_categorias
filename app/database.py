2b9124f2d0d917be096f1c5bafede8cf601bd544
JQ <julianq397@gmail.com>
2026-09-17 10:46:51 -0500

    Cambio en la BD para trabajo de documentacion

--- app/database.py
+++ app/database.py
@@ -1,20 +1,95 @@
 products_db: list[dict] = [
-    {"id": 1, "name": "Product 1", "category": "Laptops", "price": 10.99, "stock": 100, "available": True},
-    {"id": 2, "name": "Product 2", "category": "Cameras", "price": 15.99, "stock": 50, "available": True},
-    {"id": 3, "name": "Product 3", "category": "Laptops", "price": 20.99, "stock": 0, "available": False},
-    {"id": 4, "name": "Product 4", "category": "Cameras", "price": 25.99, "stock": 30, "available": True},
-    {"id": 5, "name": "Product 5", "category": "Laptops", "price": 30.99, "stock": 20, "available": True},
+    {
+        "id": 1,
+        "name": "Product 1",
+        "price": 10.99,
+        "stock": 100,
+        "category_id": 1
+    },
+    {
+        "id": 2,
+        "name": "Product 2",
+        "price": 15.99,
+        "stock": 50,
+        "category_id": 2
+    },
+    {
+        "id": 3,
+        "name": "Product 3",
+        "price": 20.99,
+        "stock": 0,
+        "category_id": 1
+    },
+    {
+        "id": 4,
+        "name": "Product 4",
+        "price": 25.99,
+        "stock": 30,
+        "category_id": 2
+    },
+    {
+        "id": 5,
+        "name": "Product 5",
+        "price": 30.99,
+        "stock": 20,
+        "category_id": 1
+    }
 ]
 
-category_db: list[dict] =[
-    {"id":1,"name":"Laptops","description":"Computadora portátil.","active":True},
-    {"id":2,"name":"Smartphones","description":"Teléfono celular inteligente.","active":True},
-    {"id":3,"name":"Tablets","description":"Tableta táctil.","active":True},
-    {"id":4,"name":"Headphones","description":"Audífonos para música.","active":True},
-    {"id":5,"name":"Monitors","description":"Pantalla para computadora.","active":True},
-    {"id":6,"name":"Keyboards","description":"Teclado para computadora.","active":True},
-    {"id":7,"name":"Mice","description":"Ratón para computadora.","active":False},
-    {"id":8,"name":"Smartwatches","description":"Reloj inteligente.","active":False},
-    {"id":9,"name":"Speakers","description":"Parlantes para audio.","active":False}
 
+category_db: list[dict] = [
+    {
+        "id": 1,
+        "name": "Laptops",
+        "description": "Computadora portátil.",
+        "active": True
+    },
+    {
+        "id": 2,
+        "name": "Smartphones",
+        "description": "Teléfono celular inteligente.",
+        "active": True
+    },
+    {
+        "id": 3,
+        "name": "Tablets",
+        "description": "Tableta táctil.",
+        "active": True
+    },
+    {
+        "id": 4,
+        "name": "Headphones",
+        "description": "Audífonos para música.",
+        "active": True
+    },
+    {
+        "id": 5,
+        "name": "Monitors",
+        "description": "Pantalla para computadora.",
+        "active": True
+    },
+    {
+        "id": 6,
+        "name": "Keyboards",
+        "description": "Teclado para computadora.",
+        "active": True
+    },
+    {
+        "id": 7,
+        "name": "Mice",
+        "description": "Ratón para computadora.",
+        "active": False
+    },
+    {
+        "id": 8,
+        "name": "Smartwatches",
+        "description": "Reloj inteligente.",
+        "active": False
+    },
+    {
+        "id": 9,
+        "name": "Speakers",
+        "description": "Parlantes para audio.",
+        "active": False
+    }
 ]
